8962776a9c180c1370507b594614764502b9d5fc
badrelbaz12 <badrelbaz12@gmail.com>
2024-12-25 15:05:50 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
           fontSize="lg"
           _hover={{ textDecoration: "none", color:"blueTheme.navLinkActive",transition:"all .5s ease-in-out" }}
         >
-        TAOUFIK BOUSSEMOUSSE {"</>"} {" "}
+        BADR ELBAZ {"</>"} {" "}
         </Link>
 
         <HStack as="nav" className="header-media" spacing="5" >
