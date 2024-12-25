96337fa41e3a8d632ff2af658804b15f94c74dd6
badrelbaz12 <badrelbaz12@gmail.com>
2024-12-25 14:34:51 +0100

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,6 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import translationEN from "./local/en.json";
 import translationFR from "./local/fr.json";
-import translationFR from "./local/de.json";
 
 const resources = {
   en: {
@@ -11,9 +10,6 @@ const resources = {
   },
   fr: {
     translation: translationFR,
-  },
-  de: {
-    translation: translationDE,
   }
 };
 
